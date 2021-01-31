f3b9a8b75d8bcdb76fc89858edac61fc18ff971d
Nithin Narayanan <nithin.narayanan@teranet.ca>
2021-01-31 23:38:37 +0530

    demo comments changes

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,29 +1,36 @@
 (function(){
-    const {pathname} = window.location;
-    const alertText = "{{$alert_text}}";
- 
-    function CheckURI(uri='',condition){
-        switch(condition){
-            case 'contains': return pathname.includes(uri)
-            case'equals': return `/${uri}` === pathname;
-            case 'starts_with':return pathname.startsWith(`/${uri}`)
-            case 'end_with':return (pathname.endsWith(uri) ||   pathname.endsWith(`${uri}/`))
-        }
-    }
+	function TriggerRules(){
+				const {pathname} = window.location;
+			const alertText = "{{$alert_text}}";
+		
+			function CheckURI(uri='',condition){
+				switch(condition){
+					case 'contains': return pathname.includes(uri)
+					case'equals': return `/${uri}` === pathname;
+					case 'starts_with':return pathname.startsWith(`/${uri}`)
+					case 'end_with':return (pathname.endsWith(uri) ||   pathname.endsWith(`${uri}/`))
+				}
+			}
 
-    let positiveRulesCount = 0;
-    let negativeRulesCount = 0;
-    @foreach($triggers as $rule)
-    if(CheckURI("{{$rule['uri']}}","{{$rule['condition']}}")){
-        if({{$rule['enable_alert']}}){
-            ++positiveRulesCount;
-        }
-        else{
-            ++negativeRulesCount;
-        }
-    }
-    @endforeach
-    if(positiveRulesCount > 0 && negativeRulesCount === 0){
-        alert(alertText);
-    }
+			let positiveRulesCount = 0;
+			let negativeRulesCount = 0;
+			@foreach($triggers as $rule)
+			if(CheckURI("{{$rule['uri']}}","{{$rule['condition']}}")){
+				if({{$rule['enable_alert']}}){
+					++positiveRulesCount;
+				}
+				else{
+					++negativeRulesCount;
+				}
+			}
+			@endforeach
+			if(positiveRulesCount > 0 && negativeRulesCount === 0){
+				alert(alertText);
+			}
+	    }
+	    document.onreadystatechange = function(){
+	        if (document.readyState === "complete") {
+		        TriggerRules();
+	        }    
+        }	    
 })();
